875606ccaed9c955b71b1165db0b165c0560dc00
Shane <mentor@neotechacademy.com>
2025-06-28 23:09:33 +0300

    Lesson 01

---
+++ src/com/neotech/lesson01/TestNGDemo.java
@@ -0,0 +1,22 @@
+package com.neotech.lesson01;
+
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+	@Test
+	public void testOne() {
+		System.out.println("This is test method 1");
+	}
+
+	@Test
+	public void testTwo() {
+		System.out.println("This is test method 2");
+	}
+
+	@Test
+	public void testThree() {
+		System.out.println("This is test method 3");
+	}
+
+}
